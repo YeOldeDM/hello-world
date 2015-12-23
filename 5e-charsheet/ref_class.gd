f6ed4ecff7538854070db7053325bd4704016cbf
YeOldeDM <nines@email.com>
2015-12-23 16:00:55 -0700

    add more races/classes to tables

--- 5e-charsheet/ref_class.gd
+++ 5e-charsheet/ref_class.gd
@@ -10,14 +10,76 @@ var table = {
 		'proficiencies':
 			{
 				'saves':	['STR', 'CON'],
-				'armor':	['light','medium'],
+				'armor':	['light','medium','shields'],
 				'weapons':	['simple','martial'],
 			}, #end of proficiencies
 		}, #end of class
-	#Duplicate the above to get the rest!
 
+	#Bard
+	1:	{
+		'name': 'Bard',
+		'hit-die':	8,
+		'primary-ability':	'CHA',
+		'proficiencies':
+			{
+				'saves':	['DEX', 'CHA'],
+				'armor':	['light'],
+				'weapons':	['simple',
+							'hand crossbow', 
+							'longsword',
+							'rapier',
+							'shortsword' ],
+			}, #end of proficiencies
+		}, #end of class
 
+	#Cleric
+	2:	{
+		'name': 'Cleric',
+		'hit-die':	8,
+		'primary-ability':	'WIS',
+		'proficiencies':
+			{
+				'saves':	['WIS', 'CHA'],
+				'armor':	['light','medium','shields'],
+				'weapons':	['simple'],
+			}, #end of proficiencies
+		}, #end of class
 
-	}
+	#Druid
+	2:	{
+		'name': 'Druid',
+		'hit-die':	8,
+		'primary-ability':	'WIS',
+		'proficiencies':
+			{
+				'saves':	['INT', 'WIS'],
+				'armor':	['light','medium','shields'],
+				'weapons':	['club',
+							'dagger',
+							'dart',
+							'javelin',
+							'mace',
+							'quarterstaff',
+							'scimitar',
+							'sickle',
+							'sling',
+							'spear'],
+			}, #end of proficiencies
+		}, #end of class
+
+	#Fighter
+	3:	{
+		'name': 'Fighter',
+		'hit-die':	10,
+		'primary-ability':	'STR',
+		'proficiencies':
+			{
+				'saves':	['STR', 'CON'],
+				'armor':	['light','medium', 'heavy','shields'],
+				'weapons':	['simple','martial'],
+			}, #end of proficiencies
+		}, #end of class
+
+	} #end of table
 
 
--- 5e-charsheet/ref_race.gd
+++ 5e-charsheet/ref_race.gd
@@ -23,16 +23,63 @@ var table = {
 				'traits':	[
 							'Dwarven Toughness'
 							],
-				},
+				}, #end of subrace
 			1:	{
 				'name':		'Mountain Dwarf',
 				'ability-mod':	{'STR': 2},
 				'traits':	[
 							'Dwarven Armor Training'
 							],
-				},
-			}, #end of subrace
+				}, #end of subrace
+			}, #end of subraces
 		}, #end of race
-	}
+	
+	#Elf
+	1:	{
+		'name':		'Elf',
+		'size':		'medium',
+		'speed':	30,
+		'ability-mod':	{'DEX': 2},
+		'traits':		[
+						'Keen Senses',
+						'Fey Anscestry',
+						'Trance',
+						],
+		'languages':	['Common', 'Elven'],
+		'sub-races':	{
+			0:	{
+				'name':		'High Elf',
+				'ability-mod':	{'INT': 1},
+				'traits':	[
+							'Darkvision',
+							'Elf Weapon Training',
+							'Cantrip',
+							'Extra Language'
+							],
+				}, #end of subrace
+			1:	{
+				'name':		'Wood Elf',
+				'ability-mod':	{'WIS': 1},
+				'traits':	[
+							'Darkvision',
+							'Elf Weapon Training',
+							'Fleet of Foot',
+							'Mask of The Wild'
+							],
+				}, #end of subrace
+			2:	{
+				'name':		'Dark Elf',
+				'ability-mod':	{'CHA': 1},
+				'traits':	[
+							'Superior Darkvision',
+							'Drow Weapon Training',
+							'Sunlight Sensitivity',
+							'Drow Magic'
+							],
+				}, #end of subrace
+			} #end of subraces
+		}, #end of race
+
+	} #end of table
 
 
